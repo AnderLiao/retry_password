882b2944941ea1ff12fde9b6a29074ebe3c9e3ae
Ander Liao <andercola@gmail.com>
2020-04-21 02:14:47 +0800

    Not to show 0 remaining balance

--- retry_password.py
+++ retry_password.py
@@ -3,11 +3,12 @@ i = 3
 while True:
 	user_pw = input('Password: ')
 	if user_pw != default_pw:
-		print('You have ' + str(i-1) + ' more chances to try')
-		i -= 1
-		if i == 0:
+		if i == 1:
 			print('Sorry, you only can try again after 24 hours')
 			break
+		else:
+			print('You have ' + str(i-1) + ' more chances to try')
+			i -= 1
 	else:
 		print('Successful login!')
 		break
